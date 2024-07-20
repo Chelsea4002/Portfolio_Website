cd1ce03b97827c101aabbd9ccf5462fa8827747d
hcho71 <hcho71@myseneca.ca>
2024-07-19 21:48:09 -0400

    Updated Project and Skills components

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,3 +1,4 @@
+import { projects } from "@/constants/projects";
 import { FaRegFolder } from "react-icons/fa";
 
 export default function Projects() {
@@ -10,10 +11,62 @@ export default function Projects() {
         </div>
 
         <div 
-          className="bg-white max-w-[95%] h-[330px] rounded-3xl relative mx-auto p-6 
-          md:h-[395px]
-          lg:p-10 lg:h-[510px] lg:max-w-[100%]">
+          className="bg-white max-w-[95%] rounded-3xl relative mx-auto p-6 lg:p-10 lg:max-w-[100%] text-lg lg:text-xl"
+        >
+          <div className="w-full grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-2 px-0">
 
+            {
+              projects.map(({ id, title, description, skills, img, demo, source }) => (
+                <div
+                  key={id}
+                  className="bg-slate-100 rounded-xl py-2 px-2 h-[100%] shadow-md shadow-gray-500 hover:scale-110"
+                >
+
+                  <div className="px-2 py-1">
+                    <img 
+                      src={img} 
+                      alt={`Image of ${title}`}
+                      className="mx-auto rounded-lg border-2" 
+                    />
+                    <h3 className="mt-4 mb-4 font-extrabold text-xl lg:text-2xl text-center">{title}</h3>
+                    <p>{description}</p>
+
+                    <div className="flex flex-row flex-wrap items-start mt-4">
+                      {
+                        skills.map((skill, index) => (
+                          <div key={index} className="bg-gradient-to-r from-[#a6d2ec] via-blue-400 to-[#a6d2ec] rounded-2xl w-auto h-auto p-2 mr-2 mt-2 text-white font-extrabold">{skill}</div>
+                        ))
+                      }
+                    </div>
+
+                    <div className="flex items-center justify-center">
+                      <button
+                        className="rounded-md text-[20px] font-semibold text-white bg-[#6497D6] w-1/2 py-3 m-4
+                        hover:bg-slate-300 hover:text-[#6497D6] hover:scale-110
+                        md:text-[25px]
+                        lg:text-[30px]
+                        transition-all duration-300" 
+                      >
+                        Demo
+                      </button>
+
+                      <button
+                        className="rounded-md text-[20px] font-semibold text-white bg-[#6497D6] w-1/2 py-3 m-4
+                        hover:bg-slate-300 hover:text-[#6497D6] hover:scale-110
+                        md:text-[25px]
+                        lg:text-[30px]
+                        transition-all duration-300"
+                      >
+                        Source
+                      </button>
+                      
+                    </div>
+                  </div>
+                  
+                </div>
+              ))
+            }
+          </div>
           
         </div> 
       </section>
--- constants/projects.jsx
+++ constants/projects.jsx
@@ -1,30 +1,38 @@
 export const projects = [
     {
         id: 1,
-        title: "My Childhood Board Game Website",
-        description: "Created my first website to share my favourite childhood board game using HTML, CSS, and Javascript",
+        title: "Blue Marble Website",
+        description: "Developed my first website to share my favourite childhood board game",
         skills: ["JavaScript", "HTML", "CSS"],
-        src: "https://bluemarble-ptz5q69hq-chelsea4002s-projects.vercel.app"
+        img: "/projects/boardgame_website.png",
+        demo: "https://bluemarble-ptz5q69hq-chelsea4002s-projects.vercel.app",
+        source: "https://github.com/Chelsea4002/WEB222-Final_Assessment"
     },
     {
         id: 2,
         title: "Blog Website",
-        description: "Developed and deployed a dynamic web app using Handlebars view engine and PostgreSQL for data storage. Implemented user registration, login/logout functionality, and CRUD operations (Create, Read, Delete) for blog posts. Enhanced security with hashed passwords using bcrypt.js. Integrated MongoDB for session management and activity tracking",
+        description: "Implemented user registration, login/logout functionality, and CRUD operations for blog posts",
         skills: ["JavaScript", "CSS", "Handlebars", "Express.js", "MongoDB", "PostgreSQL"],
-        src: "https://blog-ihr4.onrender.com"
+        img: "/projects/blog_website.png",
+        demo: "https://blog-ihr4.onrender.com",
+        source: "https://github.com/Chelsea4002/WEB322-App_Assignment6"
     },
     {
         id: 3,
         title: "Museum of Art Website",
-        description: "Developed a responsive user interface utilizing React, Next.js, and Bootstrap to interact with the Metropolitan Museum of Art Collection API. Implemented features such as Favorites and Search History using Jotai state management. Addressed data integrity and usability issues, including resolving invalid object IDs and improving navigation menu behavior for smaller devices",
+        description: "Developed a responsive user interface to interact with the Metropolitan Museum of Art Collection API",
         skills: ["Next.js", "React.js", "Bootstrap", "MongoDB"],
-        src: "https://museumof-ffnigerov-chelsea4002s-projects.vercel.app"
+        img: "/projects/museum_website.png",
+        demo: "https://museumof-ffnigerov-chelsea4002s-projects.vercel.app",
+        source: "https://github.com/Chelsea4002/WEB422_Assignment6-2"
     },
     {
         id: 4,
-        title: "My Portfolio Website",
-        description: "",
+        title: "Portfolio Website",
+        description: "Created a responsive portfolio website based on my resume",
         skills: ["Next.js", "React.js", "Tailwind CSS"],
-        src: ""
+        img: "/projects/portfolio_website.png",
+        demo: "http://localhost:3000",
+        source: "https://github.com/Chelsea4002/portfolio_website"
     }
 ];
