293de22b11677685e1b416e335575c5499debaf0
hcho71 <hcho71@myseneca.ca>
2024-07-21 16:38:18 -0400

    Updated Hero section (changed the design and made it responsive)

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,28 @@ module.exports = {
       scrollMargin: {
         'navbar': '64px', // Adjust this value to match your navbar height
       },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(10) infinite alternate, blink .7s infinite"
+      }
     },
   },
   plugins: [],
