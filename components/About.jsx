ab71779b3f5660e7646a370485728d67f1f04e30
hcho71 <hcho71@myseneca.ca>
2024-07-21 17:01:06 -0400

    Updated padding for about component

--- components/About.jsx
+++ components/About.jsx
@@ -24,39 +24,32 @@ export default function About() {
           <FiUser className="-ml-1 mt-1" />
         </div>
 
-        <div 
-          className="bg-white max-w-[95%] h-[320px] rounded-3xl relative mx-auto p-6 
-          md:h-[385px]
-          lg:p-10 lg:h-[510px] lg:max-w-[100%]"
-        >
+        <div className="bg-white max-w-[95%] rounded-3xl relative mx-auto p-4 lg:max-w-[100%]">
+          <div className="flex flex-col items-center md:flex-row lg:flex-row">
+            <Image
+              src="/profile2.png"
+              width={250}
+              height={250}
+              alt="Picture of Chelsea"
+              className="rounded-full w-[200px] h-[200px] mt-3 md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] object-cover"
+            />
 
-          <div className="-mt-6">
-            <div className="flex flex-col items-center md:flex-row lg:flex-row">
-              <Image
-                src="/profile2.png"
-                width={250}
-                height={250}
-                alt="Picture of Chelsea"
-                className="rounded-full w-[200px] h-[200px] mt-3 md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] object-cover"
-              />
+            <p className="text-[16px] text-center md:text-[20px] lg:text-[28px] pr-2 pl-2 md:pr-4 md:pl-4">
+              I am Chelsea Cho, an entry-level web developer candidate from Toronto, Canada.
+            </p>
+          </div>
 
-              <p className="text-[16px] text-center md:text-[20px] lg:text-[28px]">
-                I am Chelsea Cho, an entry-level web developer candidate from Toronto, Canada.
-              </p>
-            </div>
-
-            <div className="justify-start block">
-              <button 
-                className="rounded-3xl text-[18px] font-semibold text-white bg-[#6497D6] w-[105%] mt-6 -ml-[2.5%]
-                hover:bg-slate-300 hover:text-[#6497D6] 
-                md:text-[25px]
-                lg:text-[30px]
-                transition-all duration-300"
-                onClick={() => { downloadFileAtURL(PDF_FILE_URL) }}
-              >
-                Download CV
-              </button>
-            </div>
+          <div className="justify-start block">
+            <button 
+              className="rounded-3xl text-[18px] font-semibold text-white bg-[#6497D6] w-[97%] ml-[1.5%] mt-5
+              hover:bg-slate-300 hover:text-[#6497D6] 
+              md:text-[25px]
+              lg:text-[30px]
+              transition-all duration-300"
+              onClick={() => { downloadFileAtURL(PDF_FILE_URL) }}
+            >
+              Download CV
+            </button>
           </div>
         </div> 
       </section>
